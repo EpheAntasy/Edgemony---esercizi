f69cc30f37c6372f600756e5c025279409453b92
ValentinaUrzi <valentina.urzi@gmail.com>
2023-01-11 17:17:17 +0100

    esercizio event

---
+++ 11-01-2023/cards.js
@@ -0,0 +1,14 @@
+export const imgBg = [
+    "./assets/img/deserto.jpg",
+    "./assets/img/montagna.jpg",
+    "./assets/img/road.jpg",
+    "./assets/img/mare.jpg",
+]
+
+export const cardTitle = [
+    "Deserto?", "Montagna?", "On the Road?", "Mare?"
+]
+
+export const textColor = [
+    "#6F493A", "#4E423C", "#656C70", "#354a5d"
+]
