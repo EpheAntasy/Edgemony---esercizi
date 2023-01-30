18e556953a7e6063322540c352e799288ae15b05
ValentinaUrzi <valentina.urzi@gmail.com>
2023-01-30 16:26:00 +0100

    creazione server

---
+++ 30-01-2023/app.js
@@ -0,0 +1,13 @@
+const http = require("http")
+
+const server = http.createServer((req, res) => {
+    if (req.url === "/") /* la pagina iniziale viene indicata solo con lo "/", quindi il percorso per visualizzare la home sarà: localhost:3000/ */
+        res.end("Home page del mio server")
+    else if (req.url === "/studenti") /* il percorso della pagina degli studenti sarà: localhost:3000/studenti */
+        res.end("Questi sono gli studenti del Coding Bootamp 6")
+    else if (req.url === "/corsi") /* il percorso della pagina degli studenti sarà: localhost:3000/corsi */
+        res.end("Questi sono tutti i corsi disponibili")
+    else res.end("La pagina richiesta non e' stata trovata effettua una nuova ricerca")
+})
+
+server.listen(3000)
