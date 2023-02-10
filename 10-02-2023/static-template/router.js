3234b01016e09037f1454d8cf0295277ce41954d
ValentinaUrzi <valentina.urzi@gmail.com>
2023-02-10 16:28:02 +0100

    conversione di template statico in dinamico

---
+++ 10-02-2023/static-template/router.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const router = express.Router()
+const hbs = require("hbs")
+
+router.use(express.static("./public"))
+
+hbs.registerPartials(__dirname + "/views/partials")
+
+router.get("/", (req, res) => {
+    res.render("index",
+        {
+            pageTitle: "Home Page",
+        })
+})
+
+router.get("/contatti", (req, res) => {
+    res.render("contatti",
+        {
+            pageTitle: "Contatti",
+        })
+})
+
+router.get("/about", (req, res) => {
+    res.render("about",
+        {
+            pageTitle: "About",
+        })
+})
+
+router.get("/formazione", (req, res) => {
+    res.render("formazione",
+        {
+            pageTitle: "Formazione",
+        })
+})
+
+router.get("/web-design", (req, res) => {
+    res.render("web-design", {
+        pageTitle: "Web Design",
+    }
+    )
+})
+
+router.get("*", (req, res) => {
+    res.render("error", {
+        pageTitle: "404 - Not Found",
+    })
+})
+
+module.exports = router
